58234ff3ca1d31a0f0add2dcea96f583c1d48a23
katoshin <hatao0000000@outlook.com>
2021-09-08 00:04:34 +0900

    nextauthのセッションプロバイダを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import { Provider } from 'react-redux'
+import { Provider as ReduxProvider } from 'react-redux';
+import { Provider as NextAuthProvider } from 'next-auth/client';
+
 import { store } from '../reducks/store';
 
 export default function App({ Component, pageProps }) {
 
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <ReduxProvider store={store}>
+      <NextAuthProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </NextAuthProvider>
+    </ReduxProvider>
   )
 }
