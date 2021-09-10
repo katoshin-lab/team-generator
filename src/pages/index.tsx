9979bae9b705bed5ab17d299edf3cbba8a67a1e5
katoshin <hatao0000000@outlook.com>
2021-09-11 00:05:00 +0900

    プロバイダ別のカスタムサインインボタンテスト配置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,41 @@
+import React from "react";
+import { useSession, signOut, signIn, providers, ClientSafeProvider } from "next-auth/client";
 import Link from 'next/link';
 
-const Index = () => {
+const Index = ({ providers }):JSX.Element => {
+  const [session, loading] = useSession();
+
+  const status = (): string => {
+    if (loading) {
+      return 'loading';
+    } else if (session) {
+      return 'in session';
+    } else {
+      return 'no session';
+    }
+  }
 
   return (
     <>
-      <Link href="/test">
-        <a>Click to see current Redux State</a>
+      <Link href="/signin">
+        <a>signin</a>
       </Link>
+      <div>{ status() }</div>
+      <button onClick={() => signIn()}>signin</button>
+      <button onClick={() => signOut()}>signout</button>
+      {Object.values(providers).map((provider: ClientSafeProvider , index): JSX.Element => (
+        <button key={index} onClick={() => signIn(provider.id)}>
+          singin with {provider.name}
+        </button>
+      ))
+
+      }
     </>
   )
 }
 
-export default Index
+export async function getServerSideProps() {
+  return { props: { providers: await providers() } };
+};
+
+export default Index;
