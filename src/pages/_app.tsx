cf89bd62658446511885d115620fbd563e87b9d4
katoshin <hatao0000000@outlook.com>
2021-09-15 21:59:34 +0900

    リセットcss導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { Provider as NextAuthProvider } from 'next-auth/client';
 
 import { store } from '../reducks/store';
 
+import '../theme/reset.css'
+
 export default function App({ Component, pageProps }) {
 
   return (
