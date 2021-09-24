8a763c2ac96c6aae74cdbd0f329222bf72fa9e90
katoshin <hatao0000000@outlook.com>
2021-09-24 22:04:32 +0900

    テーマカラー設定

---
+++ src/theme/color.ts
@@ -0,0 +1,22 @@
+const color = {
+  light: {
+    main: "#f6f7d7",
+    sub: "#fbfcef",
+    second: "#3ec1d3",
+    third: "#ff9a00",
+    fourth: "#ff165d",
+    mainText: "#252525",
+    secondText: "#454545"
+  },
+  dark: {
+    main: "#1c1d06",
+    sub: "#131304",
+    second: "#3ec1d3",
+    third: "#ff9a00",
+    fourth: "#ff165d",
+    mainText: "#D1D1D1",
+    secondText: "#B1B1B1"
+  }
+};
+
+export default color;
